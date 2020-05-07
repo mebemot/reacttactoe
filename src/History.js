df3b886bf7d0892c8ca347807882ef84b72c17f0
Tom Lepard <t@Toms-MBP.connect>
2020-05-07 12:23:00 +0100

    reverse history order

--- src/History.js
+++ src/History.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import {whosTurn} from './Game'
+import React, {useState} from 'react';
+import { whosTurn } from './Game'
 
 export function History({ history, stepNumber, onClick }) {
+    const [reverse, setReverse] = useState(false)
     const [, ...actualMoves] = history
+
+
     const moves = actualMoves.map((step, index) => {
         const i = index + 1;
         var currentclass;
@@ -14,12 +17,22 @@ export function History({ history, stepNumber, onClick }) {
         }
         return (
             <li key={i}>
-                <button className={currentclass} onClick={() => onClick(i)}>{whosTurn(index)}({Math.floor(step.moveIndex / 3) + 1}, {(step.moveIndex % 3) + 1})</button>
+                 <button className={currentclass} onClick={() => onClick(i)}>{whosTurn(index)}({Math.floor(step.moveIndex / 3) + 1}, {(step.moveIndex % 3) + 1})</button>
             </li>
         );
     });
-    return (
 
-        <ol>{moves}</ol>
+    if(reverse){
+        moves.reverse()
+    }
+    return (
+        <>
+            <button className="history" onClick={()=> setReverse(!reverse)}>/\ or \/</button>
+            <ul>{moves}</ul>
+        </>
     )
+
+   
+
 }
+
