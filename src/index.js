078442a61269b265af4d1650b22a0df2bd8d7efa
Tom Lepard <t@Toms-MBP.broadband>
2020-06-30 16:23:27 +0100

    serviceWorker added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import GamesApp from "./GamesApp";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<GamesApp />, document.getElementById("root"));
+serviceWorker.register();
